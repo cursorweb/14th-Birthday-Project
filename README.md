9b159280b827964fab53e3b5f0297df61964af52
Junhao Zhang <junhaodev@gmail.com>
2021-07-08 11:25:13 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# It's My Birthday!
+Today, I turn 14! Yay!! I decided to make a little celebration about it.
+
+## About
+You can visit the website [here](...) and you will be prompted to log in. Hovering over the button will give confetti effects!
+You then log in with replit and then you are prompted to recieve a slice of cookie. The first person will receive 1/2 of the cookie, the second person will recieve 1/4 of the cookie, and so on, half of the remaining cookie. So be early to get your treats!
+
+## Screenshots
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+
+// Setup
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+
+// Log in with repl.it
+app.use((req, res, next) => {
+    res.locals.username = req.header("x-replit-user-name");
+    next();
+});
+
+
+// Statics
+app.use(express.static(__dirname + "/public"));
+
+// Views
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.use((req, res) => {
+    res.status(404).render("404");
+});
+
+
+// Run it!
+app.listen(8080);
+console.log("App running on localhost:8080");
