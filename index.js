9eced9ea5fb0cb72c7fbd51f20de9d36607f2cf4
Junhao Zhang <junhaodev@gmail.com>
2021-07-08 21:01:20 -0500

    Added site views
    Also added simple text organizer

--- index.js
+++ index.js
@@ -34,10 +34,36 @@ app.get("/login", (_, res) => {
     else res.render("login");
 });
 
+app.get("/dash", (_, res) => {
+    // if (!res.locals.username) res.redirect("/login");
+    // else
+    res.render("dashboard");
+});
+
+app.get("/create", (_, res) => {
+    // if (!res.locals.username) res.redirect("/login");
+    // else
+    // todo: redirect if made cookie to view cookie
+    res.render("make-card");
+});
+
+app.post("/create", (req, res) => {
+    // if (!res.locals.username) res.redirect("/login");
+    // else
+    res.redirect("/cookie-slice");
+});
+
 app.get("/cards", (_, res) => {
     // if (!res.locals.username) res.redirect("/login");
     // else
-        res.render("cards");
+    res.render("cards");
+});
+
+app.get("/cookie", (_, res) => {
+    // if (!res.locals.username) res.redirect("/login");
+    // else
+    // todo: redirect if not made cookie to make cookie
+    res.render("cookie-slice", { sliceNumber: 3 });
 });
 
 app.use((_, res) => {
