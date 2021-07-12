7b0e8701ce4cfd1a3639919c07f81d8c51d09057
Junhao Zhang <junhaodev@gmail.com>
2021-07-12 16:35:57 -0500

    Removed unecessary function

--- public/slice.js
+++ public/slice.js
@@ -19,18 +19,3 @@ function draw() {
     img.mask(arcMask);
     image(img, 0, 0, width, height);
 }
-
-function chip(x, y) {
-    push();
-    fill("#5a3017");
-    circle(x, y, 50);
-    pop();
-
-    // depth
-    push();
-    noFill();
-    strokeWeight(10);
-    stroke("#422108");
-    arc(x, y, 40, 40, PI / 2, PI + PI / 4);
-    pop();
-}
