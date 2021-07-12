9105d0c8c9e76dd235bc9d3ee51264c4936406cf
Junhao Zhang <junhaodev@gmail.com>
2021-07-12 17:14:18 -0500

    Simple elegant solution for a card bug

--- public/user-slice.js
+++ public/user-slice.js
@@ -1,4 +1,4 @@
-const modal = document.querySelector(".modal");
+const modal = document.querySelector(".modal-cont");
 const canvas = document.querySelector(".canvas");
 
 document.querySelectorAll(".card").forEach(el => {
