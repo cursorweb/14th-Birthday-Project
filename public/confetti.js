7c7957ec92ee289244722bddff0347929dabc0cc
Junhao Zhang <junhaodev@gmail.com>
2021-07-12 14:19:21 -0500

    Added confetti effect

--- public/confetti.js
+++ public/confetti.js
@@ -1,5 +1,21 @@
-document.querySelectorAll("button").forEach(el => {
-    el.addEventListener("mouseenter", () => {
-        
+const container = document.querySelector(".confetti-overlay");
+
+document.querySelectorAll(".btn").forEach(el => {
+    el.addEventListener("mouseover", e => {
+        const cont = document.createElement("div");
+        cont.classList.add("hover");
+
+        for (let i = 0; i < 3; i++) {
+            let confetti = document.createElement("div");
+            confetti.classList.add("confetti", `confetti-${i}`);
+            cont.appendChild(confetti);
+        }
+
+        cont.style.left = `${e.clientX}px`;
+        cont.style.top = `${e.clientY}px`;
+
+        container.appendChild(cont);
+
+        setTimeout(() => cont.remove(), 500);
     });
 });
